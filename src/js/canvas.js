3575ff1d0a858796f4bc0f19f4278656b8ae70a8
Aidan Smithers <porrige51122>
2019-12-07 10:06:54 +0000

    PixiJS initial code

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,12 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-canvas.width = innerWidth
-canvas.height = innerHeight
+import * as PIXI from 'pixi.js';
+
+// Check to see all has imported
+let type = "WebGL";
+if(!PIXI.utils.isWebGLSupported())
+  type = "canvas";
+PIXI.utils.sayHello(type);
+
+// Render application
+let app = new PIXI.Application({});
+document.body.appendChild(app.view);
+app.renderer.backgroundColor = 0x000034;
