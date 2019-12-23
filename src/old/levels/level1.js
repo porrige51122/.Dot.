5b76c6206a8cecb3306e959cb98f6f38fa4e5b72
Aidan Smithers <porrige51122>
2019-12-23 18:56:06 +0000

    Removing deprecated code

--- src/old/levels/level1.js
+++
@@ -1,9 +0,0 @@
-export const nodes = [ { s: 'A', x: 1, y: 1 },
-                       { s: 'B', x: 1, y: 2 },
-                       { s: 'B', x: 2, y: 2 },
-                       { s: 'A', x: 2, y: 3 }
-                     ];
-
-export const grid = { w: 2, h: 3 };
-
-export const message = "Click two nodes to connect them...";
